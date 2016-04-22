d3e11f0cd0044bcae2939efa23cbd85b0989bcf7
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-04-22 22:49:51 +0200

    3/2 : test for the saveInFile method

---
+++ zoo/test/save/SaveInFileTest.java
@@ -0,0 +1,46 @@
+package save;
+
+import exception.name.EmptyNameException;
+import java.io.File;
+import org.jdom2.Document;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ *
+ * @author doyenm
+ */
+public class SaveInFileTest {
+private static Save save;
+
+    @BeforeClass
+    public static void setUpClass() {
+        save = new Save();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void should_createAFileWithTheCorrectNameInTheCorrectPlace()
+            throws EmptyNameException {
+        //Given
+        org.jdom2.Document doc = new Document();
+        // When
+        save.saveInFile(doc, "./test/save/saveInFileTest.xml");
+        File expectedFile = new File("./test/save/saveInFileTest.xml");
+        // Then 
+        assertEquals(true, expectedFile.exists());
+        // We delete the test file in order to prepare the folder 
+        // for the next execution of this test
+        expectedFile.delete();
+    }
+}
