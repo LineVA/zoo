35ce131d78b55e15490d08fd5070c516273e65c6
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-05-01 22:45:24 +0200

    Comments

--- zoo/src/zoo/Zoo.java
+++ zoo/src/zoo/Zoo.java
@@ -23,6 +23,15 @@ public class Zoo {
     @Getter
     private HashMap<String, Paddock> paddocks;
 
+    /**
+     * Constructor of the object 'Zoo'
+     *
+     * @param name the name of the zoo
+     * @param width the width of the zoo
+     * @param height the height of the zoo
+     * @throws IncorrectDimensionsException throws if the width and/or the
+     * height is/are smaller than 1
+     */
     public Zoo(String name, int width, int height) throws IncorrectDimensionsException {
         this.name = name;
         if (width < 1 || height < 1) {
@@ -35,6 +44,19 @@ public class Zoo {
         paddocks = new HashMap<>();
     }
 
+    /**
+     * Method used to add a paddock to the zoo
+     *
+     * @param paddockName the name of the new paddock
+     * @param x the origin x of the new paddock
+     * @param y the origin y of the new paddock
+     * @param width the width of the new paddock
+     * @param height the height of the new paddock
+     * @throws AlreadyUsedNameException if there is already a paddock in this
+     * zoo with the same name
+     * @throws IncorrectDimensionsException if the paddock is to big for the zoo
+     * or its width and/or are smaller than 1 or its origins are sammler than 0
+     */
     public void addPaddock(String paddockName, int x, int y, int width, int height)
             throws AlreadyUsedNameException, IncorrectDimensionsException {
         if (paddocks.containsKey(paddockName)) {
@@ -57,6 +79,11 @@ public class Zoo {
         }
     }
 
+    /**
+     * Method used to list the paddocks of the zoo
+     *
+     * @return an Arraylist<String> of their names
+     */
     public ArrayList<String> listPaddock() {
         ArrayList<String> list = new ArrayList<>();
         for (HashMap.Entry<String, Paddock> entry : paddocks.entrySet()) {
@@ -65,23 +92,38 @@ public class Zoo {
         return list;
     }
 
-    public String detailedPaddock(String name) throws UnknownNameException {
-        boolean found = false;
-        final Paddock detailed;
+    /**
+     * Method used to find a paddock by its name
+     * @param name the name to search
+     * @return the paddock if it exists
+     * @throws UnknownNameException if the paddock does not exist
+     */
+    public Paddock paddockByName(String name) throws UnknownNameException {
         for (HashMap.Entry<String, Paddock> entry : paddocks.entrySet()) {
             if (entry.getKey().equals(name)) {
-                found = true;
-                detailed = entry.getValue();
-                break;
+                return entry.getValue();
             }
         }
-        if (found) {
-            return FormattingDisplay.idDisplay(name);
-        } else {
-            throw new UnknownNameException("This paddock does not exist.");
-        }
+        throw new UnknownNameException("This paddock does not exist.");
     }
 
+    /**
+     * method used to obtain detailed information about a paddock
+     * @param name the name of the paddock we search
+     * @return these information without mef
+     * @throws UnknownNameException if the searched paddock does not exist
+     */
+    public String detailedPaddock(String name) throws UnknownNameException {
+        Paddock pad = paddockByName(name);
+        return FormattingDisplay.idDisplay(name);
+    }
+
+    /**
+     * Method used to know if a paddock can be placed into the zoo without 
+     * looking for the others paddocks
+     * @param coor the coordinates of the new paddock
+     * @return true if we can set it, false if it is too big for the zoo.
+     */
     private boolean tooSmallforThisPaddock(PaddockCoordinates coor) {
         return ((coor.getX() + coor.getWidth() > this.width)
                 || (coor.getY() + coor.getHeight() > this.height));
