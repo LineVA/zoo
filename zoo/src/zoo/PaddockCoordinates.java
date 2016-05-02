aba3e6ec6b42c42c0cc436c6372da189767e65fd
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-05-02 22:59:57 +0200

    Comments

--- zoo/src/zoo/PaddockCoordinates.java
+++ zoo/src/zoo/PaddockCoordinates.java
@@ -8,20 +8,41 @@ import lombok.Getter;
  *
  * @author doyenm
  */
- @EqualsAndHashCode()
+@EqualsAndHashCode()
 public class PaddockCoordinates {
 
+    /**
+     * The x coordinate of the left and top corner
+     */
     @Getter
     private int x;
+    /**
+     * The y coordinate of the left and top corner
+     */ 
     @Getter
     private int y;
+    /**
+     * The width of the paddock
+     */
     @Getter
     private int width;
+    /**
+     * The height of the paddock
+     */
     @Getter
     private int height;
 
+    /**
+     * Constructor
+     * @param x The x coordinate of the left and top corner
+     * @param y The y coordinate of the left and top corner
+     * @param width The width of the paddock
+     * @param height The height of the paddock
+     * @throws IncorrectDimensionsException if x and/or y are lower than 0 or
+     * if the width and/or the height are lower or equals than 0
+     */
     public PaddockCoordinates(int x, int y, int width, int height) throws IncorrectDimensionsException {
-        if(x < 0 || y<0 || width<=0 || height<=0){
+        if (x < 0 || y < 0 || width <= 0 || height <= 0) {
             throw new IncorrectDimensionsException("The coordinates must be "
                     + "greater or equals than 0, the dimensions greater than 0.");
         }
@@ -30,9 +51,15 @@ public class PaddockCoordinates {
         this.width = width;
         this.height = height;
     }
-    
-    public boolean isNotCompeting(PaddockCoordinates coor){
-        boolean notCompeting = ((this.y + this.height - 1)<coor.y);
+
+    /**
+     * Calculates if two paddocks are competing
+     * @param coor the coordinates of the paddock with which the competing 
+     * is checking
+     * @return true if there is no competing, false else.
+     */
+    public boolean isNotCompeting(PaddockCoordinates coor) {
+        boolean notCompeting = ((this.y + this.height - 1) < coor.y);
         notCompeting = notCompeting || (this.y > (coor.y + coor.height - 1));
         notCompeting = notCompeting || ((this.x + this.width - 1) < coor.x);
         notCompeting = notCompeting || (this.x > (coor.x + coor.width - 1));
