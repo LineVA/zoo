afce354f016b9eae53544f50373009a1f5ec45fa
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-05-21 11:30:19 +0200

    Little omission : command line for details about a specie

---
+++ zoo/src/commandLine/commandImpl/DetailSpecie.java
@@ -0,0 +1,36 @@
+package commandLine.commandImpl;
+
+import basicGui.FormattingDisplay;
+import commandLine.Command;
+import exception.name.EmptyNameException;
+import exception.name.UnknownNameException;
+import zoo.animal.specie.Specie;
+
+/**
+ *
+ * @author doyenm
+ */
+public class DetailSpecie implements Command {
+
+    @Override
+    public String execute(String[] cmd) {
+        try {
+            Specie spec = this.zoo.findSpeciebyName(cmd[1]);
+            return (FormattingDisplay.formattingArrayList(spec.info()));
+        } catch (UnknownNameException ex) {
+            return "No paddock has this name.";
+        } catch (EmptyNameException ex) {
+            return "Please, select a name.";
+        }
+    }
+
+    @Override
+    public boolean canExecute(String[] cmd) {
+        if (cmd.length == 2) {
+            if (cmd[0].equals("spec") || cmd[0].equals("specie")) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
