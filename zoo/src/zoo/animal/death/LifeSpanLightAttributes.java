dcd3cc06e8f0f533a182e8325de3086271e8d356
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-05-21 22:12:02 +0200

    LifeSpanLightAttributes in Animal : we dot not remember the lifespan of the opposite sex

---
+++ zoo/src/zoo/animal/death/LifeSpanLightAttributes.java
@@ -0,0 +1,14 @@
+
+Expression licensePrefix is undefined on line 4, column 3 in Templates/Licenses/license-default.txt.To change this license header, choose License Headers in Project Properties.
+Expression licensePrefix is undefined on line 5, column 3 in Templates/Licenses/license-default.txt.To change this template file, choose Tools | Templates
+Expression licensePrefix is undefined on line 6, column 3 in Templates/Licenses/license-default.txt.and open the template in the editor.
+
+package zoo.animal.death;
+
+/**
+ *
+ * @author doyenm
+ */
+public class LifeSpanLightAttributes {
+
+}
