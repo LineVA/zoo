a4f2d3681e86141182685f25a8912c02d614213f
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-06-03 22:23:29 +0200

    FIX : only a XML file can describe a specie

--- zoo/src/main/InstanciateSpecies.java
+++ zoo/src/main/InstanciateSpecies.java
@@ -18,15 +18,18 @@ import zoo.animal.specie.Specie;
  * @author doyenm
  */
 public class InstanciateSpecies {
-  public static HashMap<String, Specie> instanciateSpecies(String resource) throws IOException, JDOMException {
-        HashMap<String,Specie> species = new HashMap<>();
+
+    public static HashMap<String, Specie> instanciateSpecies(String resource) throws IOException, JDOMException {
+        HashMap<String, Specie> species = new HashMap<>();
         Stream<Path> files = Files.list(Paths.get(resource));
         files.forEach((Path file) -> {
-            try {
-                if(file.endsWith(".xml")){
-                Specie tmpSpec = ParserSpecie.mainParserSpecie(file.toFile());
-                species.put(tmpSpec.getNames().getEnglishName(), tmpSpec);
-                } 
+            try{ 
+                if (file.toString().endsWith(".xml")) {
+                    Specie tmpSpec = ParserSpecie.mainParserSpecie(file.toFile());
+                    species.put(tmpSpec.getNames().getEnglishName(), tmpSpec);
+                } else {
+                    
+                }
             } catch (IOException ex) {
                 Logger.getLogger(Zoo.class.getName()).log(Level.SEVERE, null, ex);
             } catch (JDOMException ex) {
