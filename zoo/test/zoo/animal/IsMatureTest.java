75c66eade4d08384452827211fab7ca1ecc9bbe0
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-07-17 15:48:04 +0200

    Tests for the isMature method

--- zoo/test/zoo/animal/IsMatureTest.java
+++ zoo/test/zoo/animal/IsMatureTest.java
@@ -1,9 +1,128 @@
 package zoo.animal;
 
+import exception.IncorrectDataException;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import zoo.animal.reproduction.ReproductionAttributes;
+import zoo.animal.reproduction.Sex;
+
 /**
  *
  * @author doyenm
  */
 public class IsMatureTest {
+String expected;
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    @Test
+    public void shouldReturnFalseWhenTheFemaleAgeIsLowerThanItsMaturityAge() throws IncorrectDataException{
+        // Given
+        int age = 20;
+        Sex sex = Sex.FEMALE;
+        int femaleMat = 30;
+        int maleMat =10;
+        ReproductionAttributes repro = new ReproductionAttributes(femaleMat, maleMat, 0.0, 0);
+        AnimalImpl female = new AnimalImpl(null, "", null, sex, age, null,
+                null, null, 0, repro, null, null, null);
+        // When
+        boolean expectedResult = female.isMature();
+        // Then
+        assertFalse(expectedResult);
+    } 
+    
+    @Test
+    public void shouldReturnTrueWhenTheFemaleAgeEqualsToItsMaturityAge() throws IncorrectDataException{
+        // Given
+        int age = 30;
+        Sex sex = Sex.FEMALE;
+        int femaleMat = 30;
+        int maleMat =10;
+        ReproductionAttributes repro = new ReproductionAttributes(femaleMat, maleMat, 0.0, 0);
+        AnimalImpl female = new AnimalImpl(null, "", null, sex, age, null,
+                null, null, 0, repro, null, null, null);
+        // When
+        boolean expectedResult = female.isMature();
+        // Then
+        assertTrue(expectedResult);
+    } 
+    
+    @Test
+    public void shouldReturnTrueWhenTheFemaleAgeIsGreaterThanItsMaturityAge() throws IncorrectDataException{
+        // Given
+        int age = 40;
+        Sex sex = Sex.FEMALE;
+        int femaleMat = 30;
+        int maleMat =10;
+        ReproductionAttributes repro = new ReproductionAttributes(femaleMat, maleMat, 0.0, 0);
+        AnimalImpl female = new AnimalImpl(null, "", null, sex, age, null,
+                null, null, 0, repro, null, null, null);
+        // When
+        boolean expectedResult = female.isMature();
+        // Then
+        assertTrue(expectedResult);
+    } 
+    
+    @Test
+    public void shouldReturnFalseWhenTheMaleAgeIsLowerThanItsMaturityAge() throws IncorrectDataException{
+        // Given
+        int age = 20;
+        Sex sex = Sex.MALE;
+        int femaleMat = 10;
+        int maleMat =30;
+        ReproductionAttributes repro = new ReproductionAttributes(femaleMat, maleMat, 0.0, 0);
+        AnimalImpl male = new AnimalImpl(null, "", null, sex, age, null,
+                null, null, 0, repro, null, null, null);
+        // When
+        boolean expectedResult = male.isMature();
+        // Then
+        assertFalse(expectedResult);
+    } 
+    
+    @Test
+    public void shouldReturnTrueWhenTheMaleAgeIsEqualsToItsMaturityAge() throws IncorrectDataException{
+        // Given
+        int age = 30;
+        Sex sex = Sex.MALE;
+        int femaleMat = 10;
+        int maleMat =30;
+        ReproductionAttributes repro = new ReproductionAttributes(femaleMat, maleMat, 0.0, 0);
+        AnimalImpl male = new AnimalImpl(null, "", null, sex, age, null,
+                null, null, 0, repro, null, null, null);
+        // When
+        boolean expectedResult = male.isMature();
+        // Then
+        assertTrue(expectedResult);
+    } 
+    
+    @Test
+    public void shouldReturnTrueWhenTheMaleAgeIsGreaterThanItsMaturityAge() throws IncorrectDataException{
+        // Given
+        int age = 40;
+        Sex sex = Sex.MALE;
+        int femaleMat = 10;
+        int maleMat =30;
+        ReproductionAttributes repro = new ReproductionAttributes(femaleMat, maleMat, 0.0, 0);
+        AnimalImpl male = new AnimalImpl(null, "", null, sex, age, null,
+                null, null, 0, repro, null, null, null);
+        // When
+        boolean expectedResult = male.isMature();
+        // Then
+        assertTrue(expectedResult);
+    } 
 
 }
