e4680fee415a5652a891fa3fde9cdf6020d5ee87
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-07-23 23:47:50 +0200

    update tests of friendClass

--- zoo/test/backup/save/FriendClassTest.java
+++ zoo/test/backup/save/FriendClassTest.java
@@ -2,10 +2,12 @@ package backup.save;
 
 import exception.name.EmptyNameException;
 import java.io.IOException;
+import java.util.HashMap;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import zoo.Zoo;
+import zoo.animal.specie.Specie;
 
 /**
  *
@@ -20,8 +22,16 @@ public class FriendClassTest {
     public void ShouldGetNameOfZooThrowAnExceptionWhenFriendIsNull()
             throws EmptyNameException, IOException {
         // Given
+        String expectedName = "name";
+        int expectedWidth = 1;
+        int expectedHeight = 1;
+        HashMap<String, Specie> expectedSpecies = null;
+        int expectedAge = 0;
+        int expectedMonthsPerEval = 0;
+        int expectedHorizon = 0;
         Zoo zoo = new Zoo();
-        zoo.initiateZoo("name", 1, 1, null, 0, 0);
+        zoo.initiateZoo(expectedName, expectedWidth, expectedHeight, expectedSpecies, 
+                expectedAge, expectedMonthsPerEval, expectedHorizon);
         thrown.expect(NullPointerException.class);
         // When
         String actualName = zoo.getName(null);
