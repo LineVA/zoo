80c253c66fef0b485b490af50e3199bcb2998cbb
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-08-07 00:31:44 +0200

    CHANGE : the name of a paddock cannot be empty

--- zoo/src/zoo/paddock/Paddock.java
+++ zoo/src/zoo/paddock/Paddock.java
@@ -60,8 +60,13 @@ public class Paddock implements Cloneable, IPaddock {
      * @param name the name of the paddock
      * @param coor the coordinates of the paddock
      */
-    public Paddock(String name, PaddockCoordinates coor, ArrayList<IPaddock> neightbourhood) {
-        this.name = name;
+    public Paddock(String name, PaddockCoordinates coor, ArrayList<IPaddock> neightbourhood) 
+            throws EmptyNameException {
+        if (name.trim().equals("")) {
+            throw new EmptyNameException("A paddock cannot have an empty name.");
+        } else {
+            this.name = name;
+        }
         this.coordinates = coor;
         this.biome = Biome.NONE.getName();
         this.attributes = (BiomeAttributes) Biome.NONE.getAttributes().clone();
