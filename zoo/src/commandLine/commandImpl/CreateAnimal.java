5988799d4fe4a0db6ebc049b54edee428d20b429
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-08-06 23:12:49 +0200

    FIX : two animals cannot have the same name, Even if they are not in the same paddock. Works only of we add the animal by the shell

--- zoo/src/commandLine/commandImpl/CreateAnimal.java
+++ zoo/src/commandLine/commandImpl/CreateAnimal.java
@@ -5,6 +5,8 @@ import exception.IncorrectDataException;
 import exception.name.AlreadyUsedNameException;
 import exception.name.EmptyNameException;
 import exception.name.UnknownNameException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import launch.play.Play;
 import zoo.animal.Animal;
 import zoo.animal.AnimalImpl;
@@ -24,27 +26,35 @@ public class CreateAnimal implements Command {
         this.play = play;
     }
 
-      @Override
+    @Override
     public boolean hasInitiateAZoo() {
         return false;
     }
-    
+
     @Override
     public String execute(String[] cmd) {
         try {
-            IPaddock pad = this.play.getZoo().findPaddockByName(cmd[2]);
-            Specie specie = this.play.getZoo().findSpeciebyName(cmd[4]);
-            Sex sex = Sex.MALE.findByName(cmd[5]);
-            Animal animal = new AnimalImpl(specie, cmd[3], pad, sex);
-            pad.addAnimal(animal);
-            return "The animal has been created.";
-        } catch (EmptyNameException | UnknownNameException | AlreadyUsedNameException | IncorrectDataException ex) {
+            this.play.getZoo().findAnimalByName(cmd[3]);
+            return "There is already an animal with this name in the zoo.";
+        } catch (UnknownNameException ex1) {
+            try {
+                IPaddock pad = this.play.getZoo().findPaddockByName(cmd[2]);
+                Specie specie = this.play.getZoo().findSpeciebyName(cmd[4]);
+                Sex sex = Sex.MALE.findByName(cmd[5]);
+                Animal animal = new AnimalImpl(specie, cmd[3], pad, sex);
+                pad.addAnimal(animal);
+                return "The animal has been created.";
+            } catch (EmptyNameException | UnknownNameException | AlreadyUsedNameException | IncorrectDataException ex) {
+                return ex.getMessage();
+            }
+        } catch (EmptyNameException ex) {
             return ex.getMessage();
         }
     }
 
     @Override
-    public boolean canExecute(String[] cmd) {
+    public boolean canExecute(String[] cmd
+    ) {
         if (cmd.length == 6) {
             if (cmd[0].equals("animal") && cmd[1].equals("create")) {
                 return true;
