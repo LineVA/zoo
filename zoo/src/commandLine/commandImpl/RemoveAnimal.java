a5e816a10ff7ab9f14eaa230e75e7975204d5287
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-08-16 20:12:57 +0200

    RemoveAnimal &RemovePaddock

--- zoo/src/commandLine/commandImpl/RemoveAnimal.java
+++ zoo/src/commandLine/commandImpl/RemoveAnimal.java
@@ -34,7 +34,8 @@ public class RemoveAnimal implements Command {
         try {
             Animal animal = this.play.getZoo().findAnimalByName(cmd[2]);
              animal.getPaddock().removeAnimal(animal);
-            return "This animal has been removed.";
+            return this.play.getOption().getGeneralCmdBundle()
+                    .getString("ANIMAL_REMOVE_SUCCESS");
         } catch (EmptyNameException | UnknownNameException ex) {
             return ex.getMessage();
         }
--- zoo/src/commandLine/commandImpl/RemovePaddock.java
+++ zoo/src/commandLine/commandImpl/RemovePaddock.java
@@ -29,7 +29,8 @@ public class RemovePaddock implements Command{
         try {
             IPaddock pad = this.play.getZoo().findPaddockByName(cmd[2]);
             this.play.getZoo().removePaddock(pad);
-            return "This paddock has been removed.";
+            return this.play.getOption().getGeneralCmdBundle()
+                    .getString("ANIMAL_REMOVE_SUCCESS");
         } catch (EmptyNameException | UnknownNameException ex) {
             return ex.getMessage();
         }
