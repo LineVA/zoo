b1fec8e024bf00b2a34308cdc7a4f357325f2841
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-08-17 12:34:11 +0200

    1st verison of Family

---
+++ zoo/src/zoo/animal/specie/Family.java
@@ -0,0 +1,54 @@
+package zoo.animal.specie;
+
+import lombok.Getter;
+
+/**
+ *
+ * @author doyenm
+ */
+public enum Family {
+    UNKNOWN(0),
+    CANIDAE(1),
+    MUSTELIDAE(2),
+    ODOBENIDAE(3),
+    OTARIIDAE(4),
+    PHOCIDAE(5),
+    PROCYONIDAE(6),
+    URSIDAE(7),
+    FELIDAE(8),
+    LEPILEMURIDAE(9),
+    LEMURIDAE(10),
+    INDRIIDAE(11),
+    DAUBENTONIIDAE(12),
+    VHEIROGALEIDAE(13)
+//    HERPESTIDAE(9),
+//    HYENIDAE(10),
+//    VIVERRIDAe(11),
+//    PTEROPODIAE(12),
+//    CRASEONYCTERIDAE(13),
+//    EMBALLONURIDAE(14),
+//    FURIPTERIDAE(15),
+//    MEGATERMATIDAE(16),
+//    MOLOSSIDAE(17),
+//    VESPERTILIONIDAE,
+//    THYROPTERIDAE,
+//    RHINOPOMATIDAE
+//    RHINOLOPHIDAE
+//    PHYLLOSTOMIDAE
+//    NYCTERIDAE,
+//    NOCTILIONIDAE,
+//    NATALIDAE,
+//    MYZOPODIDAE,
+//    MYSTACINIDAE,
+//    MORMOOPIDAE,
+    
+    ;
+    
+    @Getter
+    private int id;
+    
+    Family(int id){
+        this.id = id;
+    }
+    
+}
