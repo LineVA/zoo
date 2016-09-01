d168831830d2f4fe4739e17bf90ab28db7e3ff80
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-09-01 22:14:48 +0200

    Return to FreePlay

--- zoo/src/launch/Main.java
+++ zoo/src/launch/Main.java
@@ -16,7 +16,7 @@ import org.jdom2.JDOMException;
 public class Main {
 
     public static void main(String[] args) throws EmptyNameException, IOException, JDOMException {
-        Play play = new TutorialPlayImpl_1();
+        Play play = new FreePlayImpl();
         MainGUI mainGUI = new MainGUI(play);
 //        Transmission transmission = new Transmission();
 //        CommandLineParser parser = new CommandLineParser(transmission);
