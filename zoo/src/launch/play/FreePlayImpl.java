fdda4d04985898a88a284a774db88826fae3b22a
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-09-08 21:30:56 +0200

    Delete unuseful imports

--- zoo/src/launch/play/FreePlayImpl.java
+++ zoo/src/launch/play/FreePlayImpl.java
@@ -2,19 +2,7 @@ package launch.play;
 
 import java.util.ResourceBundle;
 import launch.options.Option;
-import commandLine.CommandManager;
 import commandLine.commandManagerImpl.FreeCommandManager;
-import lombok.Getter;
-import lombok.Setter;
-import zoo.IZoo;
-import zoo.Zoo;
-import zoo.animal.conservation.ConservationStatus;
-import zoo.animal.feeding.Diet;
-import zoo.animal.feeding.Size;
-import zoo.animal.reproduction.Sex;
-import zoo.animal.specie.Family;
-import zoo.paddock.biome.Biome;
-import zoo.paddock.biome.Ecoregion;
 
 /**
  *
--- zoo/src/launch/play/ScenarioPlayImpl.java
+++ zoo/src/launch/play/ScenarioPlayImpl.java
@@ -1,19 +1,7 @@
 package launch.play;
 
-import commandLine.CommandManager;
 import java.util.ResourceBundle;
 import launch.options.Option;
-import lombok.Getter;
-import lombok.Setter;
-import zoo.IZoo;
-import zoo.Zoo;
-import zoo.animal.conservation.ConservationStatus;
-import zoo.animal.feeding.Diet;
-import zoo.animal.feeding.Size;
-import zoo.animal.reproduction.Sex;
-import zoo.animal.specie.Family;
-import zoo.paddock.biome.Biome;
-import zoo.paddock.biome.Ecoregion;
 
 /**
  *
