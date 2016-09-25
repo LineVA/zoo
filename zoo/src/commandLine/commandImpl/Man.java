8e2b1702ddce8b6dc19b3236a5d0778f511917b5
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-09-25 10:40:41 +0200

    Add 'man paddock'

--- zoo/src/commandLine/commandImpl/Man.java
+++ zoo/src/commandLine/commandImpl/Man.java
@@ -11,42 +11,45 @@ import utils.ReadingMan;
  * @author doyenm
  */
 public class Man implements Command {
-
+    
     Play play;
     String arg = "";
-
+    
     public Man(Play play) {
         this.play = play;
     }
-
+    
     boolean success = false;
-
+    
     @Override
     public boolean hasInitiateAZoo() {
         return false;
     }
-
+    
     @Override
     public String execute(String[] cmd) {
         try {
             if (this.arg.equals("zoo")) {
                 this.success = true;
                 return ReadingMan.load(new File("doc/man/manZoo"));
-            } 
+            } else if("pad".equals(arg) || "paddock".equals(arg)){
+                   this.success = true;
+                return ReadingMan.load(new File("doc/man/manPaddock"));
+            }           
             return "";
         } catch (IOException ex) {
             return ex.getMessage();
         }
     }
-
+    
     private boolean checkSecondArg(String cmd) {
-        if ("zoo".equals(cmd)) {
+        if ("zoo".equals(cmd) || "paddock".equals(cmd) || "pad".equals(cmd)) {
             this.arg = cmd;
-            return true;
+            return true;            
         }
         return false;
     }
-
+    
     @Override
     public boolean canExecute(String[] cmd) {
         if (cmd.length == 2) {
@@ -56,7 +59,7 @@ public class Man implements Command {
         }
         return false;
     }
-
+    
     @Override
     public boolean isSuccess() {
         return this.success;
