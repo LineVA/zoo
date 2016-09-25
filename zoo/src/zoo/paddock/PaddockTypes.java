4fa1c0cb02dda4195b57012425e1306b0b217080
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-09-25 13:42:02 +0200

    New enum : PaddockTypes

---
+++ zoo/src/zoo/paddock/PaddockTypes.java
@@ -0,0 +1,49 @@
+package zoo.paddock;
+
+import exception.name.UnknownNameException;
+import java.util.ArrayList;
+import lombok.Getter;
+
+/**
+ *
+ * @author doyenm
+ */
+public enum PaddockTypes {
+
+    UNKNOWN(0),
+    AQUARIUM(1),
+    AVIARY(2),
+    CONTACTPEN(3),
+    ISLAND(4),
+    LOWLAND(5),
+    PIT(6),
+    VIVARIUM(7);
+    
+    @Getter
+    private int id;
+
+    PaddockTypes(int id){
+        this.id = id;
+    }
+    
+    public PaddockTypes findById(int id) throws UnknownNameException {
+        for (PaddockTypes pad : PaddockTypes.values()) {
+            if (pad.getId() == id) {
+                return pad;
+            }
+        }
+        throw new UnknownNameException("POUET");
+    }
+    
+      public String toStringByLanguage(){
+        return "LIST";
+    }
+    
+    public ArrayList<String> list(){
+         ArrayList<String> list = new ArrayList<>();
+        for (PaddockTypes pad : PaddockTypes.values()) {
+            list.add("POUET");
+        }
+        return list;
+    }
+}
