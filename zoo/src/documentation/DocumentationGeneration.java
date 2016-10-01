3830c14a84eb0493e19b2079ecf530dff5316ea8
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-10-01 14:14:00 +0200

    Class to display a browser with the specified URI

---
+++ zoo/src/documentation/DocumentationGeneration.java
@@ -0,0 +1,21 @@
+package documentation;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ *
+ * @author doyenm
+ */
+public class DocumentationGeneration {
+    private static void display(URI uri) throws IOException{
+        Desktop desk = Desktop.getDesktop();
+        desk.browse(uri);
+    }
+    
+    public static void displayWikipedia(String url) throws IOException, URISyntaxException{
+        display(new URI("https://www.google.fr/"));
+    }
+}
---
+++ zoo/test/documentation/DisplayTest.java
@@ -0,0 +1,16 @@
+package documentation;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ *
+ * @author doyenm
+ */
+public class DisplayTest {
+
+    public static void main(String[] args) throws IOException, URISyntaxException{
+        DocumentationGeneration.displayWikipedia("t");
+    }
+    
+}
