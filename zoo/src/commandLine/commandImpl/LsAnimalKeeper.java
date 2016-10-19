095e042dcce53c1a2a8e939e743e3461cf0e5a29
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-10-19 22:10:14 +0200

    Add cmd to list the ak according to the paddock

--- zoo/src/commandLine/commandImpl/LsAnimalKeeper.java
+++ zoo/src/commandLine/commandImpl/LsAnimalKeeper.java
@@ -4,7 +4,10 @@ import basicGui.FormattingDisplay;
 import commandLine.Command;
 import commandLine.ReturnExec;
 import commandLine.TypeReturn;
+import exception.name.EmptyNameException;
+import exception.name.UnknownNameException;
 import launch.play.Play;
+import zoo.paddock.IPaddock;
 
 /**
  *
@@ -13,6 +16,9 @@ import launch.play.Play;
 public class LsAnimalKeeper implements Command {
 
     Play play;
+
+    String[] args;
+    // args[0] : enclos
 
     public LsAnimalKeeper(Play play) {
         this.play = play;
@@ -32,16 +38,32 @@ public class LsAnimalKeeper implements Command {
 
     @Override
     public ReturnExec execute(String[] cmd) {
-        this.success = true;
-        return new ReturnExec(
-                FormattingDisplay.formattingArrayList(this.play.getZoo().listAnimalKeeper()), 
-                TypeReturn.SUCCESS);
+        IPaddock paddock = null;
+        try {
+            if (args[0] != null) {
+                paddock = this.play.getZoo().findPaddockByName(args[0]);
+            }
+            this.success = true;
+            return new ReturnExec(
+                    FormattingDisplay.formattingArrayList(this.play.getZoo().listAnimalKeeper(paddock)),
+                    TypeReturn.SUCCESS);
+        } catch (EmptyNameException | UnknownNameException ex) {
+            return new ReturnExec(ex.getMessage(), TypeReturn.ERROR);
+        }
     }
 
     private boolean firstCmd(String[] cmd) {
         if (cmd.length == 2) {
-            if (cmd[0].equals("animalKeeper")|| "aK".equals(cmd[0]) || "ak".equals(cmd[0])) {
+            if (cmd[0].equals("animalKeeper") || "aK".equals(cmd[0]) || "ak".equals(cmd[0])) {
                 if (cmd[1].equals("ls")) {
+                    return true;
+                }
+            }
+        }
+        if (cmd.length == 4) {
+            if (cmd[0].equals("animalKeeper") || "aK".equals(cmd[0]) || "ak".equals(cmd[0])) {
+                if (cmd[1].equals("ls") && ("-p".equalsIgnoreCase(cmd[2]) || "--paddock".equalsIgnoreCase(cmd[2]))) {
+                    args[0] = cmd[3];
                     return true;
                 }
             }
@@ -51,6 +73,7 @@ public class LsAnimalKeeper implements Command {
 
     @Override
     public boolean canExecute(String[] cmd) {
+        this.args = new String[]{null};
         if (firstCmd(cmd)) {
             return true;
         }
