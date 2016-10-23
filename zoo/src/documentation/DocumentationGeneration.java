9c22245309af3de7d8543f501cc05b4303fb1451
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-10-23 14:48:35 +0200

    2/2 : Javadoc for documentation package

--- zoo/src/documentation/DocumentationGeneration.java
+++ zoo/src/documentation/DocumentationGeneration.java
@@ -14,7 +14,8 @@ public class DocumentationGeneration {
     /**
      * Open a browser with a given URL
      * @param uri the URI where to open the browser
-     * @throws IOException 
+     * @throws IOException if the user default browser is not found, or it fails to be launched, 
+     * or the default handler application failed to be launched
      */
     private static void display(URI uri) throws IOException{
         Desktop desk = Desktop.getDesktop();
@@ -24,8 +25,9 @@ public class DocumentationGeneration {
     /**
      * Open a browser with a given URI
      * @param url the String to convert in URI and where to open the browser
-     * @throws IOException
-     * @throws URISyntaxException 
+     * @throws IOException if the user default browser is not found, or it fails to be launched, 
+     * or the default handler application failed to be launched
+     * @throws URISyntaxException if the String cannot be convert to URI
      */
     public static void display(String url) throws IOException, URISyntaxException{
         display(new URI(url));
