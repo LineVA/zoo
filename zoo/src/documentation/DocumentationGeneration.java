df66c43aeea411b1c011a95a95c35012a35a0e9c
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-10-23 14:43:44 +0200

    Javadoc for package documentation

--- zoo/src/documentation/DocumentationGeneration.java
+++ zoo/src/documentation/DocumentationGeneration.java
@@ -6,15 +6,27 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- *
+ * Static methods to open a browser
  * @author doyenm
  */
 public class DocumentationGeneration {
+    
+    /**
+     * Open a browser with a given URL
+     * @param uri the URI where to open the browser
+     * @throws IOException 
+     */
     private static void display(URI uri) throws IOException{
         Desktop desk = Desktop.getDesktop();
         desk.browse(uri);
     }
     
+    /**
+     * Open a browser with a given URI
+     * @param url the String to convert in URI and where to open the browser
+     * @throws IOException
+     * @throws URISyntaxException 
+     */
     public static void display(String url) throws IOException, URISyntaxException{
         display(new URI(url));
     }
