27f82b7f6f8a331f1a7160154c4c12b616a40fe0
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-10-23 15:44:57 +0200

    toString of personalityAttributes

--- zoo/src/zoo/animal/personality/PersonalityAttributes.java
+++ zoo/src/zoo/animal/personality/PersonalityAttributes.java
@@ -9,7 +9,7 @@ import utils.Utils;
  * @author doyenm
  */
 public class PersonalityAttributes {
-    
+
     @Getter
     Double bravery;
     @Getter
@@ -21,7 +21,7 @@ public class PersonalityAttributes {
     @Getter
     Double curiosity;
 
-    public PersonalityAttributes(Double bravery, Double intelligence, Double meticulousness, 
+    public PersonalityAttributes(Double bravery, Double intelligence, Double meticulousness,
             Double greed, Double curiosity) {
         this.bravery = bravery;
         this.intelligence = intelligence;
@@ -29,10 +29,14 @@ public class PersonalityAttributes {
         this.greed = greed;
         this.curiosity = curiosity;
     }
-    
-     public String toStringByLanguage(Option option){
+
+    public String toStringByLanguage(Option option) {
         String toStr = "";
         toStr += option.getPersonalityBundle().getString("BRAVERY") + Utils.truncate(this.bravery);
+        toStr += option.getPersonalityBundle().getString("INTELLIGENCE") + Utils.truncate(this.intelligence);
+        toStr += option.getPersonalityBundle().getString("METICULOUSNESS") + Utils.truncate(this.meticulousness);
+        toStr += option.getPersonalityBundle().getString("GREED") + Utils.truncate(this.greed);
+        toStr += option.getPersonalityBundle().getString("CURIOSITY") + Utils.truncate(this.curiosity);
         return toStr;
     }
 }
