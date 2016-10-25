d4a25e856c67f2623800c853aa866636a5580fc0
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-10-25 19:29:00 +0200

    Refacto

--- zoo/src/commandLine/commandManagerImpl/FreeCommandManager.java
+++ zoo/src/commandLine/commandManagerImpl/FreeCommandManager.java
@@ -32,34 +32,49 @@ public class FreeCommandManager extends CommandManager {
         initialCommands = asList(new CreateZoo(play), new LoadZoo(play), new Options(play), new Man(play));
     }
 
+    private ReturnExec isCurrentlySaving(String[] parse) {
+        this.isSaving = false;
+        return super.getSave().confirmSaving(parse);
+    }
+
+    private ReturnExec isNotCurrentlySaving(String[] parse) {
+        for (AbstractCommand command : super.getPlayCommands()) {
+            if (command.canExecute(parse)) {
+                ReturnExec result = command.execute(parse);
+                this.isInitiate |= command.hasInitiateAZoo();
+                this.isSaving = command.isSaving();
+                return result;
+            }
+        }
+        return new ReturnExec(
+                super.getOption().getGeneralCmdBundle().getString("UNKNOWN_CMD"), TypeReturn.ERROR);
+    }
+
+    private ReturnExec hasBeenInitiate(String[] parse) {
+        if (isSaving) {
+            return isCurrentlySaving(parse);
+        }
+        return isNotCurrentlySaving(parse);
+    }
+
+    private ReturnExec hasNotBeenYetInitiate(String[] parse) {
+        for (Command command : initialCommands) {
+            if (command.canExecute(parse)) {
+                ReturnExec result = command.execute(parse);
+                this.isInitiate = command.hasInitiateAZoo();
+                return result;
+            }
+        }
+        return new ReturnExec(
+                super.getOption().getGeneralCmdBundle().getString("BEGINNING_CMD"), TypeReturn.ERROR);
+    }
+
     @Override
     public ReturnExec run(String cmd) {
         String[] parse = SplitDoubleQuotes.split(cmd);
         if (isInitiate) {
-            if (isSaving) {
-                this.isSaving = false;
-                return super.getSave().confirmSaving(parse);
-            } else {
-                for (AbstractCommand command : super.getPlayCommands()) {
-                    if (command.canExecute(parse)) {
-                        ReturnExec result = command.execute(parse);
-                        this.isInitiate |= command.hasInitiateAZoo();
-                        this.isSaving = command.isSaving();
-                        return result;
-                    }
-                }
-            }
-            return new ReturnExec(super.getOption().getGeneralCmdBundle().getString("UNKNOWN_CMD"), TypeReturn.ERROR);
-        } else {
-            for (Command command : initialCommands) {
-                if (command.canExecute(parse)) {
-                    ReturnExec result = command.execute(parse);
-                    this.isInitiate = command.hasInitiateAZoo();
-                    return result;
-                }
-            }
-            return new ReturnExec(super.getOption().getGeneralCmdBundle().getString("BEGINNING_CMD"), TypeReturn.ERROR);
-        }
-
+            return hasBeenInitiate(parse);
+        } 
+        return hasNotBeenYetInitiate(parse);
     }
 }
