40190500bc930a22a80436ef35f6cee0b4b31d37
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-10-30 15:38:53 +0100

    Clean Family

--- zoo/src/zoo/animal/specie/Family.java
+++ zoo/src/zoo/animal/specie/Family.java
@@ -2,6 +2,7 @@ package zoo.animal.specie;
 
 import exception.name.UnknownNameException;
 import java.util.ArrayList;
+import java.util.List;
 import launch.options.Option;
 import lombok.Getter;
 
@@ -68,7 +69,10 @@ public enum Family {
 
     @Getter
     private int id;
-    Option option;
+      /**
+     * The option used to know the current language
+     */
+    private Option option;
 
      public void setOption(Option option) {
         for (Family family : Family.values()) {
@@ -80,6 +84,12 @@ public enum Family {
         this.id = id;
     }
 
+      /**
+     * Find a family by its identifier
+     * @param id the identifier to search
+     * @return the corresponding diet
+     * @throws UnknownNameException if no family matches the identifier 
+     */
     public Family findById(int id) throws UnknownNameException {
         for (Family family : Family.values()) {
             if (family.getId() == id) {
@@ -90,13 +100,21 @@ public enum Family {
                 this.option.getFamilyBundle().getString("UNKNOWN_NAME"));
     }
     
+     /**
+     * toString with the current language
+     * @return the name of the family, according to the current language
+     */
      public String toStringByLanguage(){
          return this.toString();
 //        return this.option.getFamilyBundle().getString(this.toString().toUpperCase());
     }
      
-     public ArrayList<String> list() {
-        ArrayList<String> list = new ArrayList<>();
+       /**
+     * List all of the families
+     * @return the list, each item corresponding to an element of the enumeration
+     */
+     public List<String> list() {
+        List<String> list = new ArrayList<>();
         for (Family family : Family.values()) {
             list.add(family.id + " - " +
                     family.toString());
