80768bfae725ec48529d6491708e6314b7b9a9ff
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2016-12-03 10:18:27 +0100

    Extract the species for lot 1

--- zoo/src/utils/Config.java
+++ zoo/src/utils/Config.java
@@ -5,7 +5,7 @@ package utils;
  * @author doyenm
  */
 public class Config {
-    public final static String SPECIES_PATH = "resources/species";
+    public final static String SPECIES_PATH = "resources/species/lot1";
     public final static String BACKUP_PATH = "gameBackUps/";
     public final static String BACKUP_EXTENSION =".xml";
 }
